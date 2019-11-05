259821ba2110d0acc864d2baeda3fdce77e1e7f7
JUAN CARLOS Cruz <juancs02011@gmail.com>
2019-11-05 10:20:19 -0600

    Deleting show some config
    Deleting show some config at the beginning

--- DBmail-Enable.sql
+++ DBmail-Enable.sql
@@ -2,13 +2,13 @@
 -- Enable Database Mail
 --==========================================================
 
-SELECT * FROM  sys.configurations WHERE name = 'Database Mail XPs'
-GO
+--SELECT * FROM  sys.configurations WHERE name = 'Database Mail XPs'
+--GO
 /* Check for pending configurations. */
 /* You may not want to proceed if anything odd is outstanding! */
-SELECT * 
-FROM sys.configurations where value <> value_in_use;
-GO
+--SELECT * 
+--FROM sys.configurations where value <> value_in_use;
+--GO
 EXEC sp_configure 'show advanced options', '1';
 RECONFIGURE
 GO
